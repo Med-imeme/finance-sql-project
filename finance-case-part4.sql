878246240345565329f915a50fad889e7542a65f
Med-imeme <harrabiimem122017@gmail.com>
2025-07-23 04:16:54 +0100

    Update finance-case-part4.sql

--- finance-case-part4.sql
+++ finance-case-part4.sql
@@ -1,7 +1,7 @@
 --Find the top 3 companies with the most frequent "Paiement indemnité" operations.
 SELECT
-e.nom
-,count(o.type_operation) as nombre_des_operation
+   e.nom
+   ,count(o.type_operation) as nombre_des_operation
 from operations_financieres o join entreprises e on e.id=o.entreprise_id
 where o.type_operation='Paiement indemnité'
 group by e.nom
