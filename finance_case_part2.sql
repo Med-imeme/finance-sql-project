e3086b5b03255d5ec56e4d9aa6161542b78661c1
Med-imeme <harrabiimem122017@gmail.com>
2025-07-23 04:15:27 +0100

    Update finance_case_part2.sql

--- finance_case_part2.sql
+++ finance_case_part2.sql
@@ -1,16 +1,16 @@
-/*Calculate the total amount of operations per currency
+--Calculate the total amount of operations per currency
 select devise
        ,sum(montant)AS sum_of_operations_per_currency
 FROM operations_financieres
 group by devise ;
+
 --Calculate the average amount per type of financial operation.
 select type_operation
        ,avg(montant) AS average_amount
 from operations_financieres
-group by type_operation;*/
+group by type_operation;
 
 --Identify the company with the highest total amount of financial operations.
-
 select nom
       ,sum(montant) AS highest_total_amount
 from operations_financieres ,entreprises 
